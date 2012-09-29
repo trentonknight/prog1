72b5b1dc73dba30f4af2f573be0c3db35dbd7798
trentonknight <trentonknight@gmail.com>
2012-09-29 18:37:58 -0400

    fourth commit...

--- CS468_WK5_prog1.c
+++ CS468_WK5_prog1.c
@@ -11,31 +11,39 @@ int pid1 = 0,
 pid2 = 0,
 i = 0;
 
+
 pid1 = fork();
 
 if(pid1 == 0){
 
-printf("First child process [ %d ].\n", getpid());
+printf("First child process %d .\n", getpid());
 printf("\n");
 for(i = 1; i < 6; i++){
    printf("Counting...%d\n",i);
    sleep(1);
  }
-printf("First child [ %d ] will exit now.\n", getpid());
+printf("First child  %d  will exit now.\n", getpid());
 exit(0);
- }
-
 kill (pid1, SIGSTOP);
- printf("Parent process [ %d ] suspended first child process [ %d ]...\n", getppid(), getpid());
- //pid2 = fork();
+}
+pid2 = fork();
 if(pid2 == 0){
-printf("Second child process [ %d ] starting...\n", getpid());
-printf("Second child's parent process is [ %d ].\n", getppid());
-printf("Process [ %d ] will display process status:\n", getpid());
+printf("Parent process  %d  suspended first child process  %d ...\n", getppid(), getpid());
+printf("Second child process  %d  starting...\n", getpid());
+printf("Second child's parent process is  %d .\n", getppid());
+printf("Process  %d  will display process status:\n", getpid());
+execl ("/bin/ps", "ps", "-M", NULL); /* Execute ps */
 exit(0);
- }
+}
+
+//setup runtime for processes
+kill (pid2, SIGSTOP);
+printf("Second child process %d terminated\n", getpid());
 kill (pid1, SIGCONT);
-//kill (pid1, SIGINT);
-//kill (pid2, SIGINT);
+sleep(5);
+kill (pid2, SIGCONT);
+sleep(5);
+kill (pid1, SIGINT);
+kill (pid2, SIGINT);
 return 0;
 }
