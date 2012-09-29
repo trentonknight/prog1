3b9d7a08ac428222964aee7b2f2556f5f27f7dc1
trentonknight <trentonknight@gmail.com>
2012-09-29 16:09:56 -0400

    first commit.

---
+++ CS468_WK5_prog1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main()
+
+{
+int status = 0,
+pid1 = 0,
+pid2 = 0,
+returnChild_pid = 0,
+i = 0;
+
+pid1 = fork();
+
+if(pid1 == 0){
+
+printf("First child process [ %d ].\n", getpid());
+ printf("\n");
+for(i = 1; i < 6; i++){
+   printf("Counting...%d\n",i);
+   sleep(1);
+ }
+printf("First child [ %d ] will exit now.\n", getpid());
+kill (pid1, SIGSTOP);
+exit(0);
+}
+
+pid2 = fork();
+if(pid2 == 0){
+printf("Second child process [ %d ].\n", getpid());
+printf("\n");
+ for(i = 1; i < 6; i++){
+   printf("Counting...%d\n",i);
+   sleep(1);
+ }
+printf("Second child [ %d ] will exit now.\n", getpid());
+exit(0);
+}
+
+
+return 0;
+}
