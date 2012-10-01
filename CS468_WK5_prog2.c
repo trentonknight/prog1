a7a2967c7afe90abbed507b95a5e03227e133188
trentonknight <trentonknight@gmail.com>
2012-09-30 20:11:30 -0400

    got pipes working...

--- CS468_WK5_prog2.c
+++ CS468_WK5_prog2.c
@@ -5,29 +5,39 @@
 #include <signal.h>
 #include <string.h>
 #include <ctype.h>
+#include <fcntl.h>
+#include <pthread.h>
 
 #define READ 0
 #define WRITE 1
 
 void getProcessInfo();
 void getInput(char word[]);
-
+void processOne(int *fd, char word[]);
+void processTwo(int *fd);
+void processThree();
 
 int main()
 {
-int fd[2];
-int fork1_pid = 0;
-int fork2_pid = 0;
 char word[10];
-int count = 0;
- 
+int fd[2];
+int f1_pid;
+int f2_pid;
 pipe(fd);
-
-fork1_pid = fork();
-getProcessInfo();
 getInput(word);
-printf("\nword was: \"%s\" \n", word);
-fflush(stdout);
+f1_pid = fork();
+if(f1_pid == 0){
+processOne(fd,word);
+}
+sleep(5);
+kill(f1_pid, SIGSTOP);
+f2_pid = fork();
+if(f2_pid == 0){
+processTwo(fd);
+}
+sleep(5);
+kill(f2_pid, SIGINT);
+kill(f1_pid, SIGINT);
 return 0;
 }
 
@@ -48,4 +58,27 @@ if(newline != NULL)
 *newline = '\0';
 }
 }
+}
+void processOne(int *fd, char word[]){
+close(fd[READ]);
+sleep(2);
+write(fd[WRITE],word,strlen(word) + 1);
+sleep(1);
+close(fd[WRITE]);
+printf("Sending piped message...\n");
+}
+void processTwo(int *fd){
+int recieved;
+int message[100];
+close(fd[WRITE]);
+
+printf("Reading piped message...\n");
+recieved = read(fd[READ], message, 100);
+if(recieved){
+printf("\nMessage recieved from pipe: %s\n\n", message);
+}
+
+close(fd[READ]);
+}
+void processThree(){
 }
